cf5d3e5224c1bb6ddb60f9b03680e075f1ba06da
maerd19 <revolutions_333@hotmail.com>
2017-11-15 16:25:39 -0600

    Commit message

---
+++ compare-vue/src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <div id="app">
+      <div class="navbar">
+        <div class="navbar-brand">
+          <a class="navbar-item">MyCompany</a>
+
+            <button class="button navbar-burger" v-on:click="toggleNav" v-bind:class="{'is-active' : isActive}">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+          </div>
+
+          <div class="navbar-menu" v-bind:class="{'is-active' : isActive}">
+            <div class="navbar-end">
+              <router-link to="/" class="navbar-item r-item">Home</router-link>
+              <router-link to="faq" class="navbar-item r-item">Features</router-link>
+              <router-link to="faq" class="navbar-item r-item">About</router-link>
+              <router-link to="faq" class="navbar-item r-item">FAQ</router-link>
+
+              <div class="navbar-item">
+                <p class="control">
+                  <a class="button is-primary is-outlined">
+                    <span class="icon">
+                      <i class="fa fa-download"></i>
+                    </span>
+                    <span>Join Now</span>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+      </div>
+
+    <router-view></router-view>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column has-text-left">
+            <p>And this right here is a spiffy footer, where you can put stuff.</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function(){
+    return{
+      isActive: false
+    }
+  },
+  methods: {
+    toggleNav: function(){
+      this.isActive = !this.isActive;
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+@import 'mq'
+
+.navbar
+    background-color: #383838
+    a:hover
+      color:gray
+
+.navbar-brand a
+  color:#FFF
+  font-weight: bold
+
+a.r-item
+  color:#C1C1C1
+  padding: 0.5rem 1.75rem
+  +mobile
+    color: gray
+    &:hover
+      background-color: #F1F1F1
+
+.nav-toggle span
+  background-color: #C1C1C1
+
+footer
+  background-color: $primary !important
+  color: #FFF
+
+  .icon
+    color: #FFF
+    margin-left: 20px
+
+</style>
